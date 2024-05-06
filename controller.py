71f0ea33d05b1b5e6ae3c51c17e947e197f57f52
Le Loc <loclx@devcamp.edu.vn>
2024-05-06 21:48:20 +0700

    Tinh Luong

--- controller.py
+++ controller.py
@@ -1,3 +1,4 @@
+from common import to_num, cal_salary
 from models import Staff, Employee, Manager
 from dataTable import DataTable
 
@@ -24,3 +25,16 @@ class ManageEmployee:
     def searchStaffByName(self, staffName: str):
         return 
         #todo
+
+    def salary_employee(self, id = None):
+        all_staff = self.__data
+        result = 0
+        if id is None:
+            for staff in all_staff:
+                if staff.role == "0":
+                    result += cal_salary(staff)
+        else:
+            for staff in all_staff:
+                if staff.id == id:
+                    result += cal_salary(staff)
+        return result
